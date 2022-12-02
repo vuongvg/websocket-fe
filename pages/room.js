3a5ec275d884665a6e37f42f289f3a17abc1458a
vuongcdt <vuongcdt@gmail.com>
2022-12-02 20:38:08 +0700

    fix check online...

--- pages/room.js
+++ pages/room.js
@@ -119,7 +119,7 @@ export default function Room() {
       setInterval(() => {
          socketClient.emit("check", "online");
          console.log(` check`, )
-      }, 3*60* 1000); 
+      }, 2*60* 1000); 
    }, []);
 
    return (
